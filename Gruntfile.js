b32ed20e26164fda2f26d4e5a09b0b33c72371b9
Brady Vercher <brady@blazersix.com>
2014-03-14 00:17:05 -0700

    Add a couple of Grunt tasks for generating docs and sniffing code.

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,4 +38,49 @@ module.exports = function( grunt ) {
 
 	grunt.registerTask('default', ['jshint', 'watch']);
 
+	/**
+	 * Generate documentation using ApiGen.
+	 *
+	 * @link http://apigen.org/
+	 */
+	grunt.registerTask('apigen', function() {
+		var done = this.async();
+
+		grunt.util.spawn({
+			cmd: 'apigen',
+			args: [
+				'--source=.',
+				'--destination=docs',
+				'--exclude=*/.git*,*/docs/*,*/node_modules/*',
+				'--title=Simple Image Widget Documentation',
+				'--main=SimpleImageWdiget',
+				'--report=docs/_report.xml'
+			],
+			opts: { stdio: 'inherit' }
+		}, done);
+	});
+
+	/**
+	 * PHP Code Sniffer using WordPress Coding Standards.
+	 *
+	 * @link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards
+	 */
+	grunt.registerTask('phpcs', function() {
+		var done = this.async();
+
+		grunt.util.spawn({
+			cmd: 'phpcs',
+			args: [
+				'-p',
+				'-s',
+				'--standard=WordPress',
+				'--extensions=php',
+				'--ignore=*/node_modules/*',
+				'--report-file=codesniffs.txt',
+				'.'
+			],
+			opts: { stdio: 'inherit' }
+		}, done);
+	});
+
 };
